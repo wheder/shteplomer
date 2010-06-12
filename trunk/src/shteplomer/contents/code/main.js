8848f2418f5132f02997f72591fefe4677b1be42
wheder <wheder@8adff5c4-cfa6-9201-99e9-70da517167d9>
2010-06-12 23:36:02 +0000

    first working version

---
+++ trunk/src/shteplomer/contents/code/main.js
@@ -0,0 +1,58 @@
+var layout = new LinearLayout();
+plasmoid.layout = layout;
+ulabel = new Label;
+layout.addItem(ulabel);
+
+if (plasmoid.getUrl) {
+    plasmoid.aspectRatioMode = IgnoreAspectRatio;
+    var updateTimer = new QTimer;
+    updateTimer.singleShot = false; // repeat countdouwn, not only once
+    updateTimer.timeout.connect(get_temperature);
+    read_config();
+    get_temperature();
+    plasmoid.configChanged = function() {
+        read_config();
+        get_temperature();
+    }
+}
+else {
+    label.text = i18n("Can't start: No HTTP Extension available.");
+}
+
+function read_config() {
+    ulabel.font = plasmoid.readConfig("showFont")
+    updateTimer.start(plasmoid.readConfig("updateIntervalConf")*60*1000);
+}
+
+function get_temperature() {
+    var tmp = "";
+    var getJob = plasmoid.getUrl("http://teplomer.sh.cvut.cz/");
+    getJob.data.connect(recv);
+    getJob.finished.connect(fini);
+    var timedOut = 0;
+    var timeOut = new QTimer();
+    timeOut.timeout.connect(fail);
+    timeOut.start(10000); // 10 secs oughta be enough
+    timeOut.singleShot = 1;
+    function recv(job, data) {
+        if (data.length) {
+            tmp += data.valueOf();
+        }
+    }
+    function fini(job) {
+        if (!timedOut) {
+            timeOut.stop();
+            var re = new RegExp(/Aktualni teplota :(.*)(.)<\/h1>(.*)/)
+            var teplota = re.exec(tmp);
+            ulabel.text = teplota[1]
+            //print(teplota)
+        }
+    }
+    function fail() {
+        timedOut = 1;
+        ulabel.text = i18n("Can't fetch temperature! Timed out!")
+        getJob.kill();
+    }
+}
+
+//inspired in http://kde-look.org/content/show.php/HoN-Stats?content=122784
